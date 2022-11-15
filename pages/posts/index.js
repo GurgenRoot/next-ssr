8def4ce4d1d90d15ed8d09781f1bb1023cab5f0c
Albert Hovhannisyan <albert.hovhannisyan.work@gmail.com>
2022-11-15 20:07:16 +0400

    initial commit

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,7 @@
 import Layout from "../../components/Layout";
 import {useEffect, useState} from "react";
 
-export default function posts() {
+export default function Posts({posts}) {
     // const [post, setPost] = useState([]);
     //
     // useEffect(() => {
@@ -14,12 +14,21 @@ export default function posts() {
     //
     //     load()
     // }, []);
-
+    console.log(posts, 'posts')
     return(
         <Layout>
             <ul>
-                <pre>{JSON.stringify(([], null, 2))}</pre>
+                {posts.map(item => <li key={item.id}>
+                    <h3>{item.title}</h3>
+                    <p>{item.body}</p>
+                </li>)}
             </ul>
         </Layout>
     )
+}
+
+Posts.getInitialProps = async () => {
+    const response = await fetch('http://localhost:4200/posts')
+    const posts = await response.json()
+    return {posts}
 }
